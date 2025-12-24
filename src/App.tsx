78be00202c4767734dc481b8fcefac3982181401
George Mokbel <georgemokbel@gmail.com>
2025-12-24 16:09:27 -0500

    add multiple item sets for the wheel

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import SpinTheWheel from './SpinTheWheel'
-import './App.css'
+import SpinTheWheel from './SpinTheWheel';
+import './App.css';
 
 function App() {
-  const items = [
-    { name: 'Pizza', color: '#FF6B6B' },
-    { name: 'Burger', color: '#4ECDC4' },
-    { name: 'Sushi', color: '#45B7D1' },
-    { name: `Clare's Shopping Spree`, color: '#573299' },
-    { name: 'Tacos', color: '#FFA07A' },
-    { name: 'Pasta', color: '#98D8C8' },
-    { name: 'Salad', color: '#F7DC6F' },
-  ]
+    const andi = [
+        { name: 'Cinammon Roll Doll', color: '#FF6B6B' },
+        { name: 'New Backpack', color: '#4ECDC4' },
+        { name: "Tony's Sushi", color: '#45B7D1' },
+        { name: `Clare's Shopping Spree`, color: '#573299' },
+        { name: 'Amazon Giftcard', color: '#FFA07A' },
+        { name: 'New Shoes', color: '#98D8C8' },
+        { name: 'Starbucks Giftcard', color: '#F7DC6F' },
+    ];
 
-  return (
-    <div className="app">
-      <SpinTheWheel
-        items={items}
-        winningItemName="Clare's Shopping Spree"
-        numberOfRevolutions={30 + Math.floor(Math.random() * 10)}
-      />
-    </div>
-  )
+    const sloane = [
+        { name: '10 Pink Drinks', color: '#FF6B6B' },
+        { name: 'Computer Chair', color: '#4ECDC4' },
+        { name: 'Bubble Skincare', color: '#45B7D1' },
+        { name: `Movie Tickets`, color: '#573299' },
+        { name: 'Soccer Cleats', color: '#FFA07A' },
+        { name: 'Publix Giftcard', color: '#98D8C8' },
+        { name: "Dillard's Outfit", color: '#F7DC6F' },
+    ];
+
+    const hayden = [
+        { name: 'New Leotard', color: '#FF6B6B' },
+        { name: 'Cirque-de-Soleil Tickets', color: '#4ECDC4' },
+        { name: 'Water Bottle', color: '#45B7D1' },
+        { name: `Gymnastics Private`, color: '#573299' },
+        { name: 'Picasso Palette', color: '#FFA07A' },
+        { name: 'New Shoes', color: '#98D8C8' },
+        { name: 'Starbucks Giftcard', color: '#F7DC6F' },
+    ];
+
+    return (
+        <div className="app">
+            <SpinTheWheel
+                items={hayden}
+                winningItemName="Cirque-de-Soleil Tickets"
+                numberOfRevolutions={75 + Math.floor(Math.random() * 10)}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
